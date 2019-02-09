05d5cbaa06c004f4651e175fc5df4e64c3e51676
Nikhil Hira <nikhil@leancode.io>
2019-02-09 12:58:21 -0800

    a simple implementation of graphql

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Graphql
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths << Rails.root.join("app", "types")
   end
 end
